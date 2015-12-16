6dad6c3e601456eb4d21a853ff2d6629d542bbf8
John Brosnan <john@thepond.com>
2015-12-16 16:14:50 -0500

    Install ElasticSearch

---
+++ installs/install_elasticsearch_1.5.2.sh
@@ -0,0 +1,5 @@
+ curl -o elasticsearch-1.5.2.tar.gz https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-1.5.2.tar.gz
+ tar xvzf elasticsearch-1.5.2.tar.gz
+ mkdir /usr/local/sw
+ mv elasticsearch-1.5.2 /usr/local/sw
+ ln -s /usr/local/sw/elasticsearch-1.5.2/bin/elasticsearch /usr/local/bin
